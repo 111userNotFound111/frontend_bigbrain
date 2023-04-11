4fd0f84b9edb04d0ef4166fff7f24683467bbd97
cian <cianjr2438@hotmail.com>
2023-04-11 21:48:32 +1000

    create signin and signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import SignIn from './signIn';
+import SignUp from './signUp';
 
 function App () {
+  const [page, setPage] = React.useState('signup');
   return (
-    <>Let&apos;s go!</>
+    <>
+      <header>
+        <nav><a href="#" onClick={() => setPage('signup')}>Sign up</a> |<a href="#" onClick={() => setPage('signin')}>Sign in</a>
+          <hr />
+        </nav>
+      </header>
+      <main>
+        <h1>Big Brain</h1>
+        {page === 'signup'
+          ? (SignUp()
+            )
+          : (SignIn())
+        }
+      </main>
+    </>
   );
 }
 
